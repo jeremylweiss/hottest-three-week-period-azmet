1ebd6078e82ea3ab8d4ea0e4c0e31614cd7f646f
jlweiss <jlweiss@snrenet.arizona.edu>
2020-07-13 18:47:40 -0700

    working on graph

--- hottest-three-week-period-azmet.r
+++ hottest-three-week-period-azmet.r
@@ -1,7 +1,7 @@
 
 
 # This code calculates and graphs a 21-day moving average of daily maximum 
-# temperatures for AZMET Bonita station
+# temperatures for AZMET Willcox Bench station
 
 # AZMET data are at: https://cals.arizona.edu/azmet/
 
@@ -49,65 +49,112 @@ stn_data <- select(stn_data, Date, Year, Month, Day, JDay, Tmax)
 # Convert temperature from Celsius to Fahrenheit
 stn_data$Tmax <- (1.8 * stn_data$Tmax) + 32
 
-
-
-
-
-
+# Calculate moving average values
+filter_length <- 21
+stn_data["Tmax_mavg"] <- stats::filter(
+  x = stn_data$Tmax, 
+  filter = rep(1, filter_length) / filter_length,
+  method = "convolution",
+  sides = 2,
+  circular = FALSE)
+stn_data$Tmax_mavg <- as.numeric(stn_data$Tmax_mavg)
 
 
 # MAKE AND SAVE TIMESERIES PLOT --------------------
 
 
-# To help with setting axis breaks on the figures, create a function that rounds 
-# numbers to the nearest specified base
-myround <- function(x, base) {
-  base * round(x / base)
-}
-
 # To facilitate graphing interannual data that includes leap years, subtract 1
-# from 'JDay' values in a leap year. The graph will start on March 1, so this
+# from 'JDay' values in a leap year. The graph will start on May 15, so this
 # won't be an issue.
-cgdds$JDay[which(leap_year(cgdds$Year) == TRUE)] <- 
-  cgdds$JDay[which(leap_year(cgdds$Year) == TRUE)] - 1
+stn_data$JDay[which(leap_year(stn_data$Year) == TRUE)] <- 
+  stn_data$JDay[which(leap_year(stn_data$Year) == TRUE)] - 1
+
+# Calculate average maximum temperature by calendar day
+# http://www.cookbook-r.com/Manipulating_data/Calculating_a_moving_average/
+# https://vandomed.github.io/moving_averages.html
+stn_data["Tmax_davg"] <- NA
+df <- as.data.frame(stn_data$JDay)
+colnames(df) <- "JDay" 
+df["Tmax_davg"] <- NA 
+for (cal_day in min(stn_data$JDay):max(stn_data$JDay)) {
+  stn_data$Tmax_davg[which(stn_data$JDay == cal_day)] <-  
+    mean(stn_data$Tmax[which(stn_data$JDay == cal_day)], na.rm = TRUE)
+  df$Tmax_davg[which(df$JDay == cal_day)] <-  
+    mean(stn_data$Tmax[which(stn_data$JDay == cal_day)], na.rm = TRUE)
+}
+rm(cal_day)
+
+# Calculate moving average values of daily average values
+df["Tmax_mavgdavg"] <- stats::filter(
+  x = df$Tmax_davg, 
+  filter = rep(1, filter_length) / filter_length,
+  method = "convolution",
+  sides = 2,
+  circular = FALSE)
+df$Tmax_mavgdavg <- as.numeric(df$Tmax_mavgdavg)
+
+stn_data["Tmax_mavgdavg"] <- NA
+for (cal_day in min(stn_data$JDay):max(stn_data$JDay)) {
+  stn_data$Tmax_mavgdavg[which(stn_data$JDay == cal_day)] <-  
+    df$Tmax_mavgdavg[which(df$JDay == cal_day)]
+}
+rm(cal_day)
+rm(df)
 
 # Create a ggplot object for graphing cumulative growing degree days
-p <- ggplot(data = cgdds) +
+p <- ggplot(data = stn_data) +
   
-  # Add CGDD data for all years as individual time series
-  geom_line(aes(x = JDay, y = CGDDs, color = GS),
+  #  Range background shading for veraison
+  annotate(
+    "rect",
+    xmin = stn_data$JDay[which(
+      stn_data$Tmax_mavgdavg == max(
+        stn_data$Tmax_mavgdavg, na.rm = TRUE))][1] - 10,
+    xmax = stn_data$JDay[which(
+      stn_data$Tmax_mavgdavg == max(
+        stn_data$Tmax_mavgdavg, na.rm = TRUE))][1] + 10,
+    ymin = min(filter(stn_data, Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE),
+    ymax = max(filter(stn_data, Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE),
+    alpha=0.2,
+    fill = "gray50") +
+
+  # Add Tmax daily values
+  geom_point(mapping = aes(x = JDay, y = Tmax),
+             alpha = 0.5,
+             color = "gray50",
+             size = 3,
+             show.legend = TRUE) +
+
+  # Add Tmax daily average values
+  geom_line(aes(x = JDay, y = Tmax_davg),
+            color = "gray30",
             lineend = "round",
             linetype = "solid",
-            size = 1.0) +
+            size = 1.5,
+            show.legend = TRUE) +
   
   # Add the title, subtitle, axis labels, and caption
-  ggtitle("Heat Accumulation") +
+  ggtitle("Daily Maximum Temperature") +
   labs(subtitle = "AZMET Willcox Bench station",
        x = "\nDate",
-       y = "Cumulative  Growing  Degree  Days\n",
-       caption = paste0(
-         "\ndata source: AZMET (cals.arizona.edu/azmet)",
-         "\n50°F-based GDD calculation, December 1 start date")) +
-  
-  # Rename variable description and set the time series line colors
-  scale_color_brewer(name = "Growing \nSeason", palette = "Dark2") +
+       y = "°F\n",
+       caption = "\ndata source: AZMET (cals.arizona.edu/azmet)") +
   
   # Specify axis breaks, gridlines, and limits
-  scale_x_continuous(breaks = c(60, 91, 121, 152, 182, 213),
-                     labels = c("Mar", "Apr", "May", "Jun", "Jul", "Aug"),
-                     limits = c((60 - 1), (213 + 1)),
-                     expand = c(0.005, 0.0)) +
+  scale_x_continuous(
+    breaks = c(121, 135, 152, 166, 182, 196),
+    labels = c("May 1", "May 15", "Jun 1", "Jun 15", "Jul 1", "Jul 15"),
+    limits = c(134, 197),
+    expand = c(0.01, 0.01)
+  ) +
+  
   scale_y_continuous(
-    breaks = seq(
-      0,
-      myround(
-        ceiling(
-          max(filter(cgdds, JDay <= (213 + 1))$CGDDs, na.rm = TRUE)),
-        100),
-      by = 500
-      ),
-    limits = c(0, max(filter(cgdds, JDay <= (213 + 1))$CGDDs, na.rm = TRUE)),
-    expand = c(0.03, 0.0)) +
+    breaks = seq(from = 0, to = max(stn_data$Tmax, na.rm = TRUE), by = 5),
+    limits = c(
+      min(filter(stn_data, Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE),
+      max(filter(stn_data, Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE)),
+    expand = c(0.06, 0.0)
+    ) +
   
   # Further customize the figure appearance
   theme_light(base_family = "Source Sans Pro") +
@@ -141,7 +188,7 @@ p <- ggplot(data = cgdds) +
 p
 
 #  Save the figure as a .png file in the current directory
-ggsave("./cgdds-azmet-willcox-bench-cvn202007.eps",
+ggsave("./hottest-three-week-period-azmet-willcox-bench.eps",
        plot = p, device = cairo_pdf, path = NULL, scale = 1,
        width = 6, height = 4, units = "in", dpi = 300)
 
