6f81be4462eceee4cdf04e70faa48be73c2f350d
jlweiss <jlweiss@snrenet.arizona.edu>
2020-07-16 17:10:53 -0700

    graph edits

--- hottest-three-week-period-azmet.r
+++ hottest-three-week-period-azmet.r
@@ -18,6 +18,7 @@
 # Load needed libraries
 library("dplyr")
 library("ggplot2")
+library("gghighlight")
 library("lubridate")
 library("extrafont")
 
@@ -74,6 +75,12 @@ stn_data$Tmax_mavg <- as.numeric(stn_data$Tmax_mavg)
 stn_data$JDay[which(leap_year(stn_data$Year) == TRUE)] <- 
   stn_data$JDay[which(leap_year(stn_data$Year) == TRUE)] - 1
 
+# For text annotation
+months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+stn_data["Month_name"] <- NA
+stn_data$Month_name <- months[stn_data$Month]
+
 # Calculate average maximum temperature by calendar day and moving average of 
 # daily average values for the station period of record
 stn_data_avg <- as.data.frame(seq(from = 1, to = 365))
@@ -95,37 +102,95 @@ stn_data_avg$Tmax_mavgdavg <- stats::filter(
   circular = FALSE)
 stn_data_avg$Tmax_mavgdavg <- as.numeric(stn_data_avg$Tmax_mavgdavg)
 
+# For text annotation
+stn_data_avg["Month_name"] <- NA
+stn_data_avg["Day"] <- NA
+for (entry in 1:length(stn_data_avg$Month_name)) {
+  stn_data_avg$Month_name[entry] <- 
+    stn_data$Month_name[which(stn_data$JDay == entry)[1]]
+  stn_data_avg$Day[entry] <- 
+    min(stn_data$Day[which(stn_data$JDay == entry)], na.rm = TRUE)
+}
+rm(entry)
+
 # Create a ggplot object for graphing daily maximum temperature values
 p <- ggplot() +
   
   # Background shading for warmest moving average value and its range
-  annotate("rect",
-           xmin = stn_data_avg$JDay[which(
-             stn_data_avg$Tmax_mavgdavg == max(
-               stn_data_avg$Tmax_mavgdavg, na.rm = TRUE
-               ))][1] - 10,
-           xmax = stn_data_avg$JDay[which(
-             stn_data_avg$Tmax_mavgdavg == max(
-               stn_data_avg$Tmax_mavgdavg, na.rm = TRUE
-               ))][1] + 10,
-           ymin = min(filter(stn_data, 
-                             Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE),
-           ymax = max(filter(stn_data, 
-                             Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE),
-           alpha=0.2,
-           fill = "gray50") +
+  geom_rect(aes(
+    xmin = stn_data_avg$JDay[which(
+      stn_data_avg$Tmax_mavgdavg == max(
+        stn_data_avg$Tmax_mavgdavg, na.rm = TRUE
+      ))][1] - 10,
+    xmax = stn_data_avg$JDay[which(
+      stn_data_avg$Tmax_mavgdavg == max(
+        stn_data_avg$Tmax_mavgdavg, na.rm = TRUE
+      ))][1] + 10,
+    ymin = min(filter(stn_data, 
+                      JDay >= 134 & JDay <= 197)$Tmax, na.rm = TRUE) - 1,
+    ymax = max(filter(stn_data, 
+                      JDay >= 134 & JDay <= 197)$Tmax, na.rm = TRUE) + 1
+      ),
+    fill = "gray40", alpha = 0.2, show.legend = FALSE) +
   
-  annotate("text",
-           x = stn_data_avg$JDay[which(
-             stn_data_avg$Tmax_mavgdavg == max(
-               stn_data_avg$Tmax_mavgdavg, na.rm = TRUE
-             ))][1] - 10,
-           y = 75,
-           label = "Some text",
-           angle = 90,
-           color = "gray40",
-           size = 10) +
-
+  # Label shaded region
+  geom_text(aes(
+    x = stn_data_avg$JDay[which(
+      stn_data_avg$Tmax_mavgdavg == max(stn_data_avg$Tmax_mavgdavg, 
+                                        na.rm = TRUE)
+      )],
+    y = min(filter(stn_data, JDay >= 134 & JDay <= 197)$Tmax, 
+            na.rm = TRUE) + 9,
+    label = "Average  Warmest  \n21-day  Period",
+    family = "Source Sans Pro", fontface = "bold", 
+    hjust = "center", vjust = "bottom"
+    ),
+    size = 10 / .pt, color = "gray40", show.legend = FALSE) +
+  
+  # Label start and end dates of shaded region
+  geom_text(aes(
+    x = c(
+      # Start date
+      stn_data_avg$JDay[which(
+        stn_data_avg$Tmax_mavgdavg == max(stn_data_avg$Tmax_mavgdavg,
+                                          na.rm = TRUE)
+        )] - 7.5,
+      # End date
+      stn_data_avg$JDay[which(
+        stn_data_avg$Tmax_mavgdavg == max(stn_data_avg$Tmax_mavgdavg, 
+                                          na.rm = TRUE)
+      )] + 8
+      ),
+    y = min(filter(stn_data, JDay >= 134 & JDay <= 197)$Tmax, na.rm = TRUE) + 5,
+    label = c(
+      paste(
+        # Start date
+        stn_data_avg$Month_name[which(
+          stn_data_avg$Tmax_mavgdavg == max(stn_data_avg$Tmax_mavgdavg,
+                                            na.rm = TRUE)
+        ) - 10],
+        stn_data_avg$Day[which(
+          stn_data_avg$Tmax_mavgdavg == max(stn_data_avg$Tmax_mavgdavg,
+                                            na.rm = TRUE)
+        ) - 10],
+        sep = " "
+      ),
+      paste(
+        # End date
+        stn_data_avg$Month_name[which(
+          stn_data_avg$Tmax_mavgdavg == max(stn_data_avg$Tmax_mavgdavg,
+                                            na.rm = TRUE)
+        ) + 10],
+        stn_data_avg$Day[which(
+          stn_data_avg$Tmax_mavgdavg == max(stn_data_avg$Tmax_mavgdavg,
+                                            na.rm = TRUE)
+        ) + 10],
+        sep = " ")), 
+    family = "Source Sans Pro", fontface = "bold", 
+    hjust = "center", vjust = "bottom"
+    ),
+    size = 10 / .pt, color = "gray40", show.legend = FALSE) +
+  
   # Add Tmax daily values
   geom_point(data = stn_data,
              mapping = aes(x = JDay, y = Tmax, shape = "circle"),
@@ -141,7 +206,7 @@ p <- ggplot() +
             color = "gray30",
             lineend = "round",
             size = 1.5) +
-#            show.legend = TRUE) +
+            #show.legend = TRUE) +
   
   guides(shape=guide_legend("Daily Values", order = 1),
          linetype=guide_legend("Daily Average", order = 2)) +
@@ -167,8 +232,8 @@ p <- ggplot() +
   scale_y_continuous(
     breaks = seq(from = 0, to = max(stn_data$Tmax, na.rm = TRUE), by = 5),
     limits = c(
-      min(filter(stn_data, Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE) - 1,
-      max(filter(stn_data, Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE)) + 1,
+      min(filter(stn_data, JDay >= 134 & JDay <= 197)$Tmax, na.rm = TRUE) - 1,
+      max(filter(stn_data, JDay >= 134 & JDay <= 197)$Tmax, na.rm = TRUE) + 1),
     expand = c(0.0, 0.0)
     ) +
   
@@ -209,97 +274,3 @@ ggsave("./hottest-three-week-period-azmet-willcox-bench.eps",
        plot = p, device = cairo_pdf, path = NULL, scale = 1,
        width = 6, height = 4, units = "in", dpi = 300)
 
-
-
-
-
-
-
-
-# Create a ggplot object for graphing daily maximum temperature values
-p <- ggplot(data = stn_data) +
-  
-  # Background shading for warmest moving average value and its range
-  annotate("rect",
-           xmin = stn_data$JDay[which(
-             stn_data$Tmax_mavgdavg == max(
-               stn_data$Tmax_mavgdavg, na.rm = TRUE
-             ))][1] - 10,
-           xmax = stn_data$JDay[which(
-             stn_data$Tmax_mavgdavg == max(
-               stn_data$Tmax_mavgdavg, na.rm = TRUE
-             ))][1] + 10,
-           ymin = min(filter(stn_data, 
-                             Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE),
-           ymax = max(filter(stn_data, 
-                             Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE),
-           alpha=0.2,
-           fill = "gray50") +
-  
-  # Add Tmax daily values
-  geom_point(mapping = aes(x = JDay, y = Tmax, color = as.factor(Year)),
-             alpha = 0.5,
-             size = 3,
-             show.legend = TRUE) +
-  
-  # Add Tmax daily average values
-  geom_line(aes(x = JDay, y = Tmax_davg),
-            color = "gray30",
-            lineend = "round",
-            linetype = "solid",
-            size = 1.5,
-            show.legend = TRUE) +
-  
-  # Add the title, subtitle, axis labels, and caption
-  ggtitle("Daily Maximum Temperature") +
-  labs(subtitle = "AZMET Willcox Bench station",
-       x = "\nDate",
-       y = "°F\n",
-       caption = "\ndata source: AZMET (cals.arizona.edu/azmet)") +
-  
-  # Specify axis breaks, gridlines, and limits
-  scale_x_continuous(
-    breaks = c(121, 135, 152, 166, 182, 196),
-    labels = c("May 1", "May 15", "Jun 1", "Jun 15", "Jul 1", "Jul 15"),
-    limits = c(134, 197),
-    expand = c(0.01, 0.01)
-  ) +
-  
-  scale_y_continuous(
-    breaks = seq(from = 0, to = max(stn_data$Tmax, na.rm = TRUE), by = 5),
-    limits = c(
-      min(filter(stn_data, Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE),
-      max(filter(stn_data, Month >= 5 & Month <= 7)$Tmax, na.rm = TRUE)),
-    expand = c(0.06, 0.0)
-  ) +
-  
-  # Further customize the figure appearance
-  theme_light(base_family = "Source Sans Pro") +
-  theme(axis.line = element_blank(),
-        axis.text.x = element_text(color = "gray40", size = 10),
-        axis.text.y = element_text(color = "gray40", size = 10),
-        axis.ticks.x.bottom = element_line(color = "gray80", size = 0.25),
-        axis.ticks.y = element_blank(),
-        axis.ticks.length.x = unit(0.0, "mm"),
-        axis.ticks.length.y = unit(0.0, "mm"),
-        axis.title.x = element_text(color = "gray40", size = 10),
-        axis.title.y = element_text(color = "gray40", size = 10),
-        legend.direction = "vertical",
-        legend.text = element_text(color = "gray40", size = 10),
-        legend.title = element_text(color = "gray40", size = 10, face = "bold"),
-        legend.position = "right",
-        panel.border = element_blank(),
-        panel.grid.major.x = element_line(color = "gray80", size = 0.25),
-        panel.grid.major.y = element_line(color = "gray80", size = 0.25),
-        panel.grid.minor.x = element_blank(),
-        panel.grid.minor.y = element_blank(),
-        plot.caption = element_text(color = "gray40", hjust = 0.0, size = 8),
-        plot.caption.position = "plot",
-        plot.margin = unit(c(1, 1 ,1, 1), "mm"),
-        plot.subtitle = (element_text(family = "Source Serif Pro", size = 12)), 
-        plot.title = (
-          element_text(face = "bold", family = "Source Serif Pro", size = 16)
-        ),
-        plot.title.position = "plot")
-
-p
